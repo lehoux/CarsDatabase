606d96d0bd3247f63a4890661e32b08a5a2afbbc
Leszek <ziege4@gmail.com>
2017-03-25 14:06:55 +0100

    table_layout

---
+++ app/src/main/java/com/example/rent/carsdatabase/Add/AddNewCarActivity.java
@@ -0,0 +1,15 @@
+package com.example.rent.carsdatabase.Add;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rent.carsdatabase.R;
+
+public class AddNewCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_car);
+    }
+}
---
+++ app/src/main/java/com/example/rent/carsdatabase/Car.java
@@ -0,0 +1,7 @@
+package com.example.rent.carsdatabase;
+
+/**
+ * Created by RENT on 2017-03-25.
+ */
+public class Car {
+}
